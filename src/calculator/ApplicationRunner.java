4f83d0e01007d3404326e036ed6d3d6000a44527
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-02-10 13:34:55 +0100

    adding buttons for numbers and operations

--- src/calculator/ApplicationRunner.java
+++ src/calculator/ApplicationRunner.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import javax.swing.*;
+import java.awt.*;
 
 class Calculator extends JFrame{
     public Calculator () {
@@ -16,14 +17,170 @@ class Calculator extends JFrame{
     }
     public void initComponents() {
         JTextField EquationTextField = new JTextField();
-        EquationTextField.setBounds(140,20, 120,30);
+        EquationTextField.setBounds(10,10, 265,30);
         add(EquationTextField);
+        /**
+         * pannel for buttons
+         */
+        JPanel panel = new JPanel(new GridLayout(4, 5));
+        panel.setBounds(10,45,265,210);
+        //panel.setLayout(new BorderLayout());
+        //panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+        add(panel);
 
-        JButton Solve = new JButton();
-        Solve.setBounds(100,100,100,30);
-        Solve.setText("Solve");
-        add(Solve);
-        Solve.addActionListener(e -> {
+        /**
+         * add buttons to it
+         */
+
+        JButton Seven = new JButton();
+        Seven.setName("Seven");
+        Seven.setBounds(20, 10, 40, 40);
+        Seven.setText("7");
+        panel.add(Seven);
+        Seven.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Seven.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Eight = new JButton();
+        Eight.setName("Eight");
+        Eight.setBounds(80, 10, 40, 40);
+        Eight.setText("8");
+        panel.add(Eight);
+        Eight.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Eight.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Nine = new JButton();
+        Nine.setName("Nine");
+        Nine.setBounds(140, 10, 40, 40);
+        Nine.setText("9");
+        panel.add(Nine);
+        Nine.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Nine.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Divide = new JButton();
+        Divide.setName("Divide");
+        Divide.setBounds(200, 10, 40, 40);
+        Divide.setText("/");
+        panel.add(Divide);
+        Divide.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Divide.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Four = new JButton();
+        Four.setName("Four");
+        Four.setBounds(20, 55, 40, 40);
+        Four.setText("4");
+        panel.add(Four);
+        Four.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Four.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Five = new JButton();
+        Five.setName("Five");
+        Five.setBounds(80, 55, 40, 40);
+        Five.setText("5");
+        panel.add(Five);
+        Five.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Five.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Six = new JButton();
+        Six.setName("Six");
+        Six.setBounds(140, 55, 40, 40);
+        Six.setText("6");
+        panel.add(Six);
+        Six.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Six.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Multiply = new JButton();
+        Multiply.setName("Multiply");
+        Multiply.setBounds(200, 55, 40, 40);
+        Multiply.setText("*");
+        panel.add(Multiply);
+        Multiply.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Multiply.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton One = new JButton();
+        One.setName("One");
+        One.setBounds(20, 100, 40, 40);
+        One.setText("1");
+        panel.add(One);
+        One.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += One.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Two = new JButton();
+        Two.setName("Two");
+        Two.setBounds(80, 100, 40, 40);
+        Two.setText("2");
+        panel.add(Two);
+        Two.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Two.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Three = new JButton();
+        Three.setName("Three");
+        Three.setBounds(140, 100, 40, 40);
+        Three.setText("3");
+        panel.add(Three);
+        Three.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Three.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Add = new JButton();
+        Add.setName("Add");
+        Add.setBounds(200, 100, 40, 40);
+        Add.setText("+");
+        panel.add(Add);
+        Add.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Add.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Zero = new JButton();
+
+        Zero.setName("Zero");
+        Zero.setText("0");
+        Zero.setBounds(80, 155, 40, 40);
+        panel.add(Zero);
+        Zero.addActionListener(e -> {
+            String content = EquationTextField.getText();
+            content += Zero.getText();
+            EquationTextField.setText(content);
+        });
+
+        JButton Equals = new JButton();
+        Equals.setBounds(140, 155, 40, 40);
+        Equals.setText("=");
+        panel.add(Equals);
+        Equals.addActionListener(e -> {
             String equation = EquationTextField.getText();
             int[] array = new int[2];
             String[] arrayOfString = equation.split("[+-/*]");
@@ -53,6 +210,18 @@ class Calculator extends JFrame{
             }
             EquationTextField.setText(equation);
         });
+
+        JButton Subtract = new JButton();
+        Subtract.setName("Subtract");
+        Subtract.setBounds(200, 155, 40, 40);
+        Subtract.setText("-");
+        panel.add(Subtract);
+        Subtract.addActionListener(e -> {
+            String content = EquationTextField.getText();
+             content += Subtract.getText();
+            EquationTextField.setText(content);
+        });
+
     }
 }
 public class ApplicationRunner {
