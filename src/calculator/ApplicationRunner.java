160374555aedfcbc0c262ca92b4524077f64d316
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-02-14 13:44:27 +0100

    the calculator has now much more functionalities!

--- src/calculator/ApplicationRunner.java
+++ src/calculator/ApplicationRunner.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Stack;
-
 class Calculator extends JFrame{
     public Calculator () {
         super("Calculator");
@@ -18,47 +17,107 @@ class Calculator extends JFrame{
         setVisible(true);
     }
     public static String PostfixConverter (String equation) {
+        final String divideSymbol = "\u00F7";
+        final String multiplySymbol = "\u00D7";
+        final String addSymbol = "\u002B";
+        final String subtractSymbol = "-";
         String result = "";
         String operator = "";
         for (int i = 0; i < equation.length(); i++) {
-            char c = equation.charAt(i);
+            String c = equation.substring(i, i + 1);
             if (i == 0) {
-                result += equation.charAt(i);
+                result += '(';
+                for (int j = 0; j < equation.length(); j++) {
+                    char c2 = equation.charAt(j);
+                    if (Character.isDigit(c2)) {
+                        result += equation.charAt(j);
+                    } else if (c2 == '.') {
+                        result += equation.charAt(j);
+                    } else {
+                        break;
+                    }
+
+                }
+                result += ')';
             }
-                if (c == 'x') {
-                    result += equation.charAt(i + 1);
-                    result += c;
-                } else if (c == '÷') {
-                    result += equation.charAt(i + 1);
-                    result += c;
-                } else if (c == '-'){
-                    result += equation.charAt(i + 1);
-                    operator += c;
-                } else if (c == '+') {
-                    result += equation.charAt(i + 1);
+            if (c.equals(multiplySymbol) || c.equals(divideSymbol)) {
+                result += '(';
+                for (int j = i + 1; j <equation.length() ; j++) {
+                    char c2 = equation.charAt(j);
+                    if (Character.isDigit(c2)) {
+                        result += equation.charAt(j);
+                    } else if (c2 == '.') {
+                        result += equation.charAt(j);
+                    } else {
+                        break;
+                    }
+                }
+                result += ')';
+                result += c;
+            } else if (c.equals(subtractSymbol) || c.equals(addSymbol)) {
+                result += '(';
+                for (int j = i + 1; j < equation.length(); j++) {
+                    char c2 = equation.charAt(j);
+                    if (Character.isDigit(c2)) {
+                        result += equation.charAt(j);
+                    } else if (c2 == '.') {
+                        result += equation.charAt(j);
+                    } else {
+                        break;
+                    }
+                }
+                result += ')';
+                if (i < equation.length() - 3) {
+                    String s = equation.substring(i + 2, i + 3);
+                    if (s.equals(divideSymbol) || s.equals(multiplySymbol)) {
+                        operator += c;
+                    } else {
+                        result += c;
+                    }
+                } else {
                     operator += c;
                 }
             }
-
-        result += operator;
+        }
+        if (operator.length() > 0) {
+            String var = "";
+            for (int i = 0; i < operator.length(); i++) {
+                var += operator.charAt(operator.length() - 1 - i);
+            }
+            result += var;
+        }
         return result;
     }
     public static double calculate(String equation) {
+        String member = "";
+        final String divideSymbol = "\u00F7";
+        final String multiplySymbol = "\u00D7";
+        final String addSymbol = "\u002B";
+        final String subtractSymbol = "-";
         Stack<Double> stack = new Stack<>();
         for (int i = 0; i < equation.length(); i++) {
-            char c = equation.charAt(i);
-            if (Character.isDigit(c)) {
-                stack.push((double) (c - '0'));
-            }  else
-            {
+            String c = equation.substring(i, i + 1);
+            if (c.equals("(")) {
+                for (int j = i + 1; j <equation.length() ; j++) {
+                    char c2 = equation.charAt(j);
+                    if (Character.isDigit(c2)) {
+                        member += equation.charAt(j);
+                    } else if (c2 == '.') {
+                        member += equation.charAt(j);
+                    }else break;
+                }
+                stack.push(Double.parseDouble(member));
+                member = "";
+            } else if (c.equals(addSymbol) || c.equals(subtractSymbol) || c.equals(divideSymbol) || c.equals(multiplySymbol)) {
+
                 double val1 = stack.pop();
                 double val2 = stack.pop();
 
                 switch (c) {
-                    case '+' -> stack.push(val2 + val1);
-                    case '-' -> stack.push(val2 - val1);
-                    case '÷' -> stack.push(val2 / val1);
-                    case 'x' -> stack.push(val2 * val1);
+                    case addSymbol -> stack.push(val2 + val1);
+                    case subtractSymbol -> stack.push(val2 - val1);
+                    case divideSymbol -> stack.push(val2 / val1);
+                    case multiplySymbol -> stack.push(val2 * val1);
                 }
             }
         }
@@ -69,6 +128,7 @@ class Calculator extends JFrame{
          * first label
          */
         JLabel ResultLabel = new JLabel("0", JLabel.RIGHT);
+        ResultLabel.setName("ResultLabel");
         ResultLabel.setBounds(10,10, 270,30);
         add(ResultLabel);
 
@@ -81,6 +141,7 @@ class Calculator extends JFrame{
          */
         JLabel EquationLabel = new JLabel("", JLabel.RIGHT);
         EquationLabel.setBounds(10,60, 270,30);
+        EquationLabel.setName("EquationLabel");
         add(EquationLabel);
         EquationLabel.setForeground(Color.BLUE);
         /**
@@ -108,16 +169,6 @@ class Calculator extends JFrame{
             ResultLabel.setText("0");
             EquationLabel.setText("");
         });
-/*        JTextField EquationTextField = new JTextField();
-        EquationTextField.setBounds(10,10, 265,30);
-        EquationTextField.setName("EquationTextField");
-        add(EquationTextField);*/
-        /**
-         * pannel for buttons
-         */
-/*        JPanel panel = new JPanel(new GridLayout(4, 5));
-        panel.setBounds(10,45,265,210);
-        add(panel);*/
 
         /**
          * add buttons to it
@@ -130,11 +181,6 @@ class Calculator extends JFrame{
         add(Seven);
         Seven.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Seven.getText();
-            } else {
-                content += Seven.getText();
-            }*/
             content += Seven.getText();
             EquationLabel.setText(content);
         });
@@ -146,11 +192,6 @@ class Calculator extends JFrame{
         add(Eight);
         Eight.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Eight.getText();
-            } else {
-                content += Eight.getText();
-            }*/
             content += Eight.getText();
             EquationLabel.setText(content);
         });
@@ -162,11 +203,6 @@ class Calculator extends JFrame{
         add(Nine);
         Nine.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Nine.getText();
-            } else {
-                content += Nine.getText();
-            }*/
             content += Nine.getText();
             EquationLabel.setText(content);
         });
@@ -189,11 +225,6 @@ class Calculator extends JFrame{
         add(Four);
         Four.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Four.getText();
-            } else {
-                content += Four.getText();
-            }*/
             content += Four.getText();
             EquationLabel.setText(content);
         });
@@ -205,11 +236,6 @@ class Calculator extends JFrame{
         add(Five);
         Five.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Five.getText();
-            } else {
-                content += Five.getText();
-            }*/
             content += Five.getText();
             EquationLabel.setText(content);
         });
@@ -221,11 +247,6 @@ class Calculator extends JFrame{
         add(Six);
         Six.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Six.getText();
-            } else {
-                content += Six.getText();
-            }*/
             content += Six.getText();
             EquationLabel.setText(content);
         });
@@ -233,7 +254,7 @@ class Calculator extends JFrame{
         JButton Multiply = new JButton();
         Multiply.setName("Multiply");
         Multiply.setBounds(220, 260, 60, 40);
-        Multiply.setText("x");
+        Multiply.setText("×");
         add(Multiply);
         Multiply.addActionListener(e -> {
             String content = EquationLabel.getText();
@@ -248,11 +269,6 @@ class Calculator extends JFrame{
         add(One);
         One.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = One.getText();
-            } else {
-                content += One.getText();
-            }*/
             content += One.getText();
             EquationLabel.setText(content);
         });
@@ -264,11 +280,6 @@ class Calculator extends JFrame{
         add(Two);
         Two.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Two.getText();
-            } else {
-                content += Two.getText();
-            }*/
             content += Two.getText();
             EquationLabel.setText(content);
         });
@@ -280,11 +291,6 @@ class Calculator extends JFrame{
         add(Three);
         Three.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Three.getText();
-            } else {
-                content += Three.getText();
-            }*/
             content += Three.getText();
             EquationLabel.setText(content);
         });
@@ -308,7 +314,6 @@ class Calculator extends JFrame{
         add(Dot);
         Dot.addActionListener(e -> {
             String content = EquationLabel.getText();
-            //content += ",";
             content += Dot.getText();
             EquationLabel.setText(content);
         });
@@ -321,11 +326,6 @@ class Calculator extends JFrame{
         add(Zero);
         Zero.addActionListener(e -> {
             String content = EquationLabel.getText();
-/*            if (content.equals("0")) {
-                content = Zero.getText();
-            } else {
-                content += Zero.getText();
-            }*/
             content += Zero.getText();
             EquationLabel.setText(content);
         });
@@ -339,37 +339,8 @@ class Calculator extends JFrame{
             String equation = EquationLabel.getText();
             double result;
             result = calculate(PostfixConverter(equation));
-/*            String[] arrayOfString = equation.split("[÷x+-]");
-            double[] array = new double[arrayOfString.length];
-            for (int i = 0; i < array.length; i++) {
-                array[i] = Double.parseDouble(arrayOfString[i]);
-            }
-            ArrayList<Character> arrayOfOperators = new ArrayList<>();
-*//*            array[0] = Integer.parseInt(arrayOfString[0]);
-            array[1] = Integer.parseInt(arrayOfString[1]);*//*
-            String operator = "";
-
-            for (int i = 0; i < equation.length(); i++) {
-                operator += equation.charAt(i);
-                if (operator.matches("[0-9]||.[0-9]")) {
-                    operator = "";
-                } else if (operator.matches("[÷x+-]")){
-                    switch (operator) {
-                        case "+" -> arrayOfOperators.add('+');
-                        case "-" -> arrayOfOperators.add('-');
-                        case "÷" -> arrayOfOperators.add('/');
-                        case "x" -> arrayOfOperators.add('*');
-*//*                        case "+" -> result = array[0] + array[1];
-                        case "-" -> result = array[0] - array[1];
-                        case "÷" -> result = array[0] / array[1];
-                        case "x" -> result = array[0] * array[1];*//*
-
-                    }
-                    //operator = "";
-                }
-            }*/
             String stringResult = String.format("%.1f", result);
-            String[] arrayWithoutComma = stringResult.split(",");
+            String[] arrayWithoutComma = stringResult.split(",");//\.
             if (arrayWithoutComma[1].equals("0")) {
                 equation = arrayWithoutComma[0];
             } else {
@@ -393,6 +364,8 @@ class Calculator extends JFrame{
         });
     }
 }
+
+
 public class ApplicationRunner {
     public static void main(String[] args) {
         new Calculator();
